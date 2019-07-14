316365bb49d5a0ad6758eddfc7852b45d8e20a59
devin.desktop <devinobowen@gmail.com>
2019-07-14 00:17:14 -0400

    Updates

---
+++ precache-manifest.ab518b0d14cddfcc012b664236d75251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f64dd1e74d1d980cbcb00e019732f38",
+    "url": "/pagestest/index.html"
+  },
+  {
+    "revision": "e655c938bb77afb26bed",
+    "url": "/pagestest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pagestest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e655c938bb77afb26bed",
+    "url": "/pagestest/static/js/main.f05f5f35.chunk.js"
+  },
+  {
+    "revision": "baf79f50d7ce0c69764e",
+    "url": "/pagestest/static/js/runtime~main.8292529e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pagestest/static/media/logo.5d5d9eef.svg"
+  }
+]);
